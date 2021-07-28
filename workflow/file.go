5d78145a862dec5acc67b07851537b46c21cbc03
amnonbb <amnonbb@gmail.com>
2021-07-28 18:43:29 +0300

    add file type to end of file name

--- workflow/file.go
+++ workflow/file.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Bnei-Baruch/wf-srv/common"
 	"github.com/rs/zerolog/log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -29,6 +30,7 @@ type Files struct {
 
 func (f *Files) SaveFile() error {
 
+	var FileType string
 	TimeStamp := time.Now().UnixNano()
 	DateNow := time.Now().Format("2006-01-02")
 	Year := strings.Split(DateNow, "-")[0] + "/"
@@ -37,6 +39,13 @@ func (f *Files) SaveFile() error {
 	FileName := f.FileName
 	SavePath := common.FilesPath + Year + Month
 
+	reg, err := regexp.Compile("[^-_a-zA-Z0-9]+")
+	if err != nil {
+		FileType = "type-unknown"
+	} else {
+		FileType = strings.ToLower(reg.ReplaceAllString(f.FileType, ""))
+	}
+
 	// Make directory
 	if _, err := os.Stat(SavePath); os.IsNotExist(err) {
 		err = os.Mkdir(SavePath, os.ModeDir|0755)
@@ -44,7 +53,7 @@ func (f *Files) SaveFile() error {
 		return err
 	}
 
-	FilePath := SavePath + FileName + "_" + FileId + "." + f.Extension
+	FilePath := SavePath + FileName + "_" + FileId + "_" + FileType + "." + f.Extension
 
 	f.FileID = FileId
 	f.Date = DateNow
@@ -52,7 +61,7 @@ func (f *Files) SaveFile() error {
 	f.Props["timestamp"] = TimeStamp
 	f.Props["url"] = FilePath
 
-	err := os.Rename(common.UploadPath+f.Sha1, FilePath)
+	err = os.Rename(common.UploadPath+f.Sha1, FilePath)
 	if err != nil {
 		log.Error().Str("source", "FILES").Err(err).Msg("SaveFile: failed to move file")
 		return err
